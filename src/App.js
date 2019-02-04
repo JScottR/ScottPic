3288006a68ba7e279ac71a0b30d551fc9d845a0e
JScott <john.s.richards@colorado.edu>
2019-02-03 18:31:48 -0700

    Inital Set up of routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from "./Components/Home";
+import Gallery from "./Components/gallery";
+import About from "./Components/about";
+import Contact from "./Components/contact";
+import Blog from "./Components/blog";
+import error from "./Components/error";
+import Navigation from "./Components/navigation";
+
+
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <BrowserRouter>
+      <div>
+        <Navigation/>
+        <Switch>
+          <Route path="/" component={Home} exact/>
+          <Route path="/gallery" component={Gallery}/>
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact}/>
+          <Route path="/blog" component={Blog}/>
+          <Route component={error}/>
+        </Switch>
       </div>
+      </BrowserRouter>
     );
   }
 }
